42705a3bfd01722fcab46f754f71013542acf398
Dujiashan0711 <shandu7652@gmail.com>
2021-09-10 21:09:38 +0800

    Initial Commit

---
+++ classic04/classic04App.swift
@@ -0,0 +1,17 @@
+//
+//  classic04App.swift
+//  classic04
+//
+//  Created by 暨大附中２ on 2021/9/10.
+//
+
+import SwiftUI
+
+@main
+struct classic04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
