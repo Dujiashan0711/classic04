3232250c5c06217f32710f5a1dd758dea3798d42
Dujiashan0711 <shandu7652@gmail.com>
2021-09-10 22:02:06 +0800

    滑動版面

--- classic04/ContentView.swift
+++ classic04/ContentView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        TabView{
+        RoundedRectangle(cornerRadius: 30)
+            .fill(Color(#colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)))
             .padding()
+        RoundedRectangle(cornerRadius: 30)
+            .fill(Color(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)))
+            .padding()
+        RoundedRectangle(cornerRadius: 30)
+            .fill(Color(#colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)))
+            .padding()
+        }
+        .tabViewStyle(PageTabViewStyle())
     }
 }
 
